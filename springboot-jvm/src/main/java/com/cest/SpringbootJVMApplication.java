97911346a6c351396778450f8e335a2ba48ef5bd
786638185@qq.com <aa19910101>
2021-03-31 20:00:43 +0800

    jvm

--- springboot-jvm/src/main/java/com/cest/SpringbootJVMApplication.java
+++ springboot-jvm/src/main/java/com/cest/SpringbootJVMApplication.java
@@ -1,11 +1,12 @@
 package com.cest;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootJVMApplication {
 
     public static void main(String[] args) {
-        appl
+        SpringApplication.run(SpringbootJVMApplication.class,args);
     }
 }
