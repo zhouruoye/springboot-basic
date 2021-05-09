5181e6b85dbb2bd12f71978ca48bc5f092ec88d8
786638185@qq.com <aa19910101>
2021-05-09 21:40:40 +0800

    数组

---
+++ springboot-arithmetic/src/main/java/com/cest/arrays/genericDataStructures/Main.java
@@ -0,0 +1,16 @@
+package com.cest.arrays.genericDataStructures;
+public class Main {
+
+    public static void main(String[] args) {
+        Array array = new Array(20);
+        for (int i = 0; i < 10; i++) {
+            array.addFirst(i);
+            array.addLast(i);
+        }
+
+        array.remove(0);
+
+        System.out.println("success");
+
+    }
+}
