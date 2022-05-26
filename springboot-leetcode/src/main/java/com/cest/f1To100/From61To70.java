d04ca970cc63a6c36c2cf572c24bdcbc4b15005a
zhouruoye <786638185@qq.com>
2022-05-26 18:59:29 +0800

    leetcode动态规划

--- springboot-leetcode/src/main/java/com/cest/f1To100/From61To70.java
+++ springboot-leetcode/src/main/java/com/cest/f1To100/From61To70.java
@@ -5,8 +5,19 @@ public class From61To70 {
     public static void main(String[] args) {
 //        int i = climbStairs2(45);
 
-        uniquePaths(3, 2);
-        System.out.println(111);
+//        uniquePaths(3, 2);
+
+        int[][] obstacleGrid = new int[2][3];
+
+        obstacleGrid[0][0] = 0;
+        obstacleGrid[0][1] = 0;
+        obstacleGrid[0][2] = 0;
+
+        obstacleGrid[1][0] = 0;
+        obstacleGrid[1][1] = 1;
+        obstacleGrid[1][2] = 0;
+        int i = uniquePathsWithObstacles(obstacleGrid);
+        System.out.println(i);
         //1836311903
     }
 
@@ -50,6 +61,61 @@ public class From61To70 {
         }
 
         return pathTimes[m-1][n-1];
+    }
+
+    /**
+     * 63. 不同路径 II
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+     * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+     * 输出：2
+     * 解释：3x3 网格的正中间有一个障碍物。
+     * 从左上角到右下角一共有 2 条不同的路径：
+     * 1. 向右 -> 向右 -> 向下 -> 向下
+     * 2. 向下 -> 向下 -> 向右 -> 向右
+     *
+     * 动态规划
+     * 两边有石头 石头及之后的路径直接为0
+     * 其他位置有石头 次数0
+     * 如果当前位置没有石头 nums[i][j] = nums[i][j - 1] + nums[i - 1][j]
+     * @param obstacleGrid
+     * @return
+     */
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] nums = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            if(obstacleGrid[i][0] != 1) {
+                nums[i][0] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(obstacleGrid[0][i] != 1) {
+                nums[0][i] = 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    nums[i][j] = 0;
+                } else {
+                    nums[i][j] = nums[i][j - 1] + nums[i - 1][j];
+                 }
+            }
+        }
+
+        return nums[m-1][n-1];
     }
 
     /**
