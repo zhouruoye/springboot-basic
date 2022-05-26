06b5aa6b840f1f24ad2a7f8f6766fe0fb0db2698
zhouruoye <786638185@qq.com>
2022-05-26 18:32:57 +0800

    leetcode动态规划

--- springboot-leetcode/src/main/java/com/cest/f1To100/From61To70.java
+++ springboot-leetcode/src/main/java/com/cest/f1To100/From61To70.java
@@ -3,9 +3,53 @@ package com.cest.f1To100;
 public class From61To70 {
 
     public static void main(String[] args) {
-        int i = climbStairs2(45);
-        System.out.println(i);
+//        int i = climbStairs2(45);
+
+        uniquePaths(3, 2);
+        System.out.println(111);
         //1836311903
+    }
+
+
+    /**
+     * 62. 不同路径
+     * 一个机器人位于一个 m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+     * 问总共有多少条不同的路径？
+     * 示例 2：
+     * 输入：m = 3, n = 2
+     * 输出：3
+     * 解释：
+     * 从左上角开始，总共有 3 条路径可以到达右下角。
+     * 1. 向右 -> 向下 -> 向下
+     * 2. 向下 -> 向下 -> 向右
+     * 3. 向下 -> 向右 -> 向下
+     *
+     * 动态规划
+     * pathTimes[i][j] = pathTimes[i][j-1] + pathTimes[i-1][j]
+     */
+    public static int uniquePaths(int m, int n) {
+        if(m == 1 || n == 1) {
+            return 1;
+        }
+
+        int[][] pathTimes = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            pathTimes[i][0] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            pathTimes[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                pathTimes[i][j] = pathTimes[i][j-1] + pathTimes[i-1][j];
+            }
+        }
+
+        return pathTimes[m-1][n-1];
     }
 
     /**
